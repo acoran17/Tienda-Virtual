774dd55780130436dec35f5522f111774ad44488
Acoran17 <cristo27603@gmail.com>
2024-10-18 16:25:29 +0100

    Mixed y Service incompleto

---
+++ src/code/services/Session.php
@@ -0,0 +1,79 @@
+<?php
+class Sesion extends Service{
+  private String $name;
+  private int $nSessions;
+
+  public function __construct(string $name, int $nSessions)
+  {
+    $this->name = $name;
+    $this->nSessions = $nSessions;
+  }
+  
+  /**
+   * add Sessions
+   *
+   * @return void
+   */
+  public function addSessions () {
+    $this->nSession++;
+  }
+  
+  /**
+   * delete Sessions
+   *
+   * @return void
+   */
+  public function deleteSessions () {
+    $this->nSession--;
+  }
+  
+  /**
+   * show Sessions
+   *
+   * @return void
+   */
+  public function  showSessions () {
+    return $this->nSession + " " +$this->name;
+  }
+
+    /**
+   * Return session name.
+   *
+   * @return string
+   */
+  public function getName(): string
+  {
+    return $this->name;
+  }
+
+  /**
+   * Set session name.
+   *
+   * @param string $name
+   */
+  public function setName(string $name)
+  {
+    $this->name = $name;
+  }
+
+      /**
+   * Return session number of sessions.
+   *
+   * @return string
+   */
+  public function getnSession(): int
+  {
+    return $this->nSessions;
+  }
+
+    /**
+   * Set session number of sessions.
+   *
+   * @param string $name
+   */
+  public function setnSession(int $nSessions)
+  {
+    $this->name = $name;
+  }
+
+}
